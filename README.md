79b526e23c1defddad7a039cc109a2fdab4cd918
尘心 <smilewupeng@gmail.com>
2022-04-06 15:47:18 +0800

    feat: webpack 基本配置

---
+++ README.md
@@ -0,0 +1,10 @@
+# webpack - 面试
+
+## 基本配置
+* mode
+* output
+* module
+* devServer
+
+
+## 高级配置
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./assets/style/style1.css"
+import "./assets/style/style2.less"
+
+import { sum } from './math'
+
+const sumRes = sum(10, 20)
+console.log('sumRes', sumRes)
+
+console.log('window.ENV', ENV)
